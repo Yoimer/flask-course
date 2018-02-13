2ed09bd6cd72b2989fe4195fce4a425dd8003025
Yoimer <ydrf19@gmail.com>
2018-02-13 01:03:17 -0400

    added render_template on __init__.py and index.html

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     #return "Hi there, how ya doing?"
-    return render_template("index.html")
+    return render_template("index.html", title="Epic Tutorial", paragraph="wow I am learning so much great stuff!")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
